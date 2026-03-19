5ae33a1481a9936b27af0354004aa1fe6685c5b2
Klymenko Dmytro <kdv.fav@gmail.com>
2026-03-19 15:22:54 +0200

    homework_09_task_2

---
+++ assignments/homework_09_task_2/task_2.py
@@ -0,0 +1,41 @@
+import sys
+
+for i in sys.path:
+    print(i)
+
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv\Scripts\python.exe C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\assignments\homework_09_task_2\task_2.py
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\assignments\homework_09_task_2
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_display
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\python313.zip
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\DLLs
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\Lib
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv\Lib\site-packages
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_matplotlib_backend
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_altair_backend
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_plotly_backend
+
+sys.path.append(r'C:\Users\kdvfa\Desktop')
+
+for i in sys.path:
+    print(i)
+
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv\Scripts\python.exe C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\assignments\homework_09_task_2\task_2.py
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\assignments\homework_09_task_2
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_display
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\python313.zip
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\DLLs
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0\Lib
+#C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.13_3.13.3312.0_x64__qbz5n2kfra8p0
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv
+#C:\Users\kdvfa\PycharmProjects\PY-Course-Victor-Nikoriak-23_02\.venv\Lib\site-packages
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_matplotlib_backend
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_altair_backend
+#C:\Program Files\JetBrains\PyCharm 2025.3.3\plugins\python-ce\helpers\pycharm_plotly_backend
+#C:\Users\kdvfa\Desktop
+
+# Тепер я можу імпортувати модулі які знаходяться на Рабочому столі, також якщо мені потрібно
+# додати дерикторію на постійно, я можу додати її в змінних серидовищя PATHONPATH у параметрах.
