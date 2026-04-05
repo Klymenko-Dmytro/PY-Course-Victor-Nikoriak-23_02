03fd40df91f5871c93290ecfd9a1ffe87e3719ef
Klymenko Dmytro <kdv.fav@gmail.com>
2026-04-05 13:03:34 +0300

    homework_15_task_1

---
+++ assignments/homework_15_task_1/task_1.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def talk(self):
+        return print(f"Hello, my name is {self.first_name} {self.last_name} and I’m {self.age} years old!")
+
+
+greeting = Person("Dmytro", "Klymenko", 38)
+
+greeting.talk()
