e1c1c079199a36af99c42f36f46cb8f6e2b96524
Klymenko Dmytro <kdv.fav@gmail.com>
2026-06-20 22:32:06 +0300

    homework_40

--- assignments/homework_39/notes_app/notes/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import notes_list
-
-urlpatterns = [
-    path('list/', notes_list, name='notes_list'),
-]
--- assignments/homework_40/notes_app/notes_app/settings.py
+++ assignments/homework_40/notes_app/notes_app/settings.py
@@ -72,13 +72,29 @@ WSGI_APPLICATION = "notes_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+'''
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notes_db',          # Назва створеної бази даних
+        'USER': 'postgres',      # Твій користувач PostgreSQL (зазвичай postgres)
+        'PASSWORD': 'manax108',  # Твій пароль від PostgreSQL
+        'HOST': 'localhost',         # Сервер бази даних
+        'PORT': '5433',              # Стандартний порт PostgreSQL
+    }
+}
+
 
 
 # Password validation
